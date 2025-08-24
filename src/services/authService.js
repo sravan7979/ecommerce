9b5a42630c6991f9c1703a1ff0b912025e34ddbe
sravan7979 <sravankumar@MATURUs-MacBook-Air.local>
2025-08-24 20:57:47 +0530

    anth updation 2

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/back1";
+const API_URL = "http://localhost:8080";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/auth/login`, { username, password });
